74101615333b54f0f5c01ed5786cc266ae1acdcb
Molly <qwqw-3330512@gmail.com>
2025-04-24 15:37:01 +0300

    feat(app): init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello, World!");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+});
